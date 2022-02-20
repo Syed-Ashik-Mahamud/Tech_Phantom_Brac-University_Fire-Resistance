b8d876ddbef3d07f70aa447e4818fa9ae6cb28db
Syed Ashik Mahamud (Joy) <79223733+Syed-Ashik-Mahamud@users.noreply.github.com>
2022-02-19 21:05:55 -0600

    Add files via upload

---
+++ Arduino Code and diagram/fire_alram_system_gsm_/fire_alram_system_gsm_.ino
@@ -0,0 +1,97 @@
+#include <LiquidCrystal_I2C.h>
+
+#include <SPI.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <DHT.h>
+
+
+char auth[] = "rHxO4fy3kmeJPdfcZMd-s8T9WuE21d3b"; //Enter the Auth code which was send by Blink
+
+char ssid[] = "Sanjida";  //Enter your WIFI Name
+char pass[] = "sanjida1991";  //Enter your WIFI Password
+LiquidCrystal_I2C lcd(0x27,20,4);
+#define DHTPIN 2         
+
+
+#define DHTTYPE DHT11     
+#define buzzerPin 16 
+
+DHT dht(DHTPIN, DHTTYPE);
+BlynkTimer timer;
+int gasSensor;
+void sendSensor()
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); 
+ 
+  lcd.setCursor(0,0);
+  lcd.print("Temp: ");
+  lcd.print(t);
+  lcd.print("'C");
+  
+  lcd.setCursor(0,1);
+  lcd.print("Humid: ");
+  lcd.print(h);
+  lcd.print("%");
+
+  gasSensor=analogRead(A0);
+  Serial.println(gasSensor);
+  
+  if(gasSensor>700)
+  {
+    digitalWrite(13,LOW);
+    digitalWrite(14,HIGH); 
+     lcd.setCursor(0, 2);
+     lcd.print("Value: ");
+     lcd.print(gasSensor);
+     lcd.setCursor(0, 3);
+     lcd.print("Gas detected");
+     tone(buzzerPin,800,40); 
+    
+  }
+  
+  if(gasSensor<700)
+  {
+      digitalWrite(13,HIGH);
+      digitalWrite(14,LOW);
+      lcd.setCursor(0, 2);
+      lcd.print("Value: ");
+      lcd.print(gasSensor);
+      lcd.setCursor(0, 3);
+      lcd.print("---No gas---");
+      noTone(buzzerPin);
+      
+  }
+
+  Blynk.virtualWrite(V1, gasSensor);
+  Blynk.virtualWrite(V5, h);  //V5 is for Humidity
+  Blynk.virtualWrite(V6, t);  //V6 is for Temperature
+  
+}
+
+void setup()
+{
+  Serial.begin(115200); // See the connection status in Serial Monitor
+  Blynk.begin(auth, ssid, pass);
+
+  dht.begin();
+  pinMode(13,OUTPUT);
+  pinMode(14,OUTPUT);
+  pinMode(buzzerPin,OUTPUT);
+
+  
+  timer.setInterval(1000L, sendSensor);
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+}
+
+void loop()
+{
+  Blynk.run(); // Initiates Blynk
+  timer.run(); // Initiates SimpleTimer
+
+ 
+  
+}
---
+++ Arduino Code and diagram/gsm/gsm.ino
@@ -0,0 +1,105 @@
+#define redled 9       //This will define "redled" as number 9 whenever it occurs again in code
+
+#define greenled 8     //This will define "greenled" as number 8 whenever it occurs again in code
+
+#define BuzzerPin 7    //This will define "BuzzerPin" as number 7 whenever it occurs again in code
+
+#define FlamePin 5     //This will define "FlamePin" as number 5 whenever it occurs again in code
+
+ 
+
+boolean Flame_Sensor_Value = HIGH;    //This variable will store the value of Flame Sensor
+
+ 
+
+void setup()  //This will run only once and at the start of the program
+
+{
+
+  /*By using pinMode function we will declare state 
+
+   * of each pin as Output/Input depending on their nature
+
+   * We use OUTPUT when we have to define the output state of the pin (0/5V)
+
+   * We use Input when we have to read
+
+   */
+
+  pinMode(BuzzerPin, OUTPUT);
+
+  pinMode(redled, OUTPUT);
+
+  pinMode(greenled, OUTPUT);
+
+  pinMode(FlamePin, INPUT);
+
+  Serial.begin(9600);   //Set the baud rate to 9600 bit per second
+
+}
+
+ 
+
+void loop()   //This will run indefinitely till the program is terminated
+
+{
+
+  Flame_Sensor_Value = digitalRead(FlamePin); //Store the value of sensor in this variable
+
+ 
+
+  if (Flame_Sensor_Value == LOW)  //check if condition matches with fire detection state
+
+  {
+
+    /*Turn on Indicators*/
+
+    digitalWrite(BuzzerPin, HIGH);    // Turn Buzzer on
+
+    digitalWrite(redled, HIGH);       // Turn Red LED on
+
+    digitalWrite(greenled, LOW);      // Turn Green LED off
+
+   
+
+    /*Responsible for sending SMS*/
+
+    Serial.println("AT+CMGF=1");                 //Selects SMS message format as TEXT
+
+    delay(1000);                                 // Delay of 1 second
+
+    Serial.println("AT+CMGS=\"+8801762259976\""); // Replace x with mobile number
+
+    Serial.println("FIRE IN THE HOUSE");         // The SMS text you want to send
+
+    Serial.println((char)26);
+
+    delay(500);
+
+   
+
+    /*Responsible for making a CALL*/
+
+    Serial.println("ATD+ +8801762259976;"); // Replace x with your mobile number
+
+    delay(5000);                          //Wait for Call to reach
+
+    Serial.println("ATH");                 //Hang up the Call
+
+  }
+
+  else
+
+  {
+
+ 
+
+    digitalWrite(BuzzerPin, LOW);          // Turn Buzzer off
+
+    digitalWrite(greenled, HIGH);          // Turn Green LED on
+
+    digitalWrite(redled, LOW);             // Turn Red LED off
+
+  }
+
+}
